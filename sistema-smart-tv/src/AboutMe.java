392bebbffa1dfb1e245401d16c8cc4e0a1a32344
caiosousaf <caiosousafernandesferreira@hotmail.com>
2024-09-01 15:14:26 -0300

    Treinando tratamento de exceções

--- sistema-smart-tv/src/AboutMe.java
+++ sistema-smart-tv/src/AboutMe.java
@@ -1,27 +1,32 @@
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class AboutMe {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        try {
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Digite seu nome");
-        String nome = scanner.next();
+            System.out.println("Digite seu nome");
+            String nome = scanner.next();
 
-        System.out.println("Digite seu sobrenome");
-        String sobrenome = scanner.next();
+            System.out.println("Digite seu sobrenome");
+            String sobrenome = scanner.next();
 
-        System.out.println("Digite sua idade");
-        int idade = scanner.nextInt();
-        
-        System.out.println("Digite sua altura EX: 1.90");
-        double altura = scanner.nextDouble();
+            System.out.println("Digite sua idade");
+            int idade = scanner.nextInt();
 
-        scanner.close();
+            System.out.println("Digite sua altura EX: 1.90");
+            double altura = scanner.nextDouble();
 
-        System.out.println("Olá, me chamo " + nome + " " + sobrenome);
-        System.out.println("Tenho " + idade);
-        System.out.println("Minha altura é " + altura + "cm");
+            scanner.close();
+
+            System.out.println("Olá, me chamo " + nome + " " + sobrenome);
+            System.out.println("Tenho " + idade);
+            System.out.println("Minha altura é " + altura + "cm");
+        } catch (InputMismatchException e) {
+            System.err.println("Os campos idade e altura precisam ser númericos");
+        }
     }
 
 }
